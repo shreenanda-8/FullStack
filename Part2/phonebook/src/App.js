4e4111c42b76a7e63bd8dcf6a82e597e3f5657a7
Shreenanda P <shreenandap@gmail.com>
2021-07-19 08:59:37 +0530

    Exercises 2.6 - 2.10.

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,35 +1,44 @@
 import React, { useState } from 'react'
-const Persons = (props)=>{
-  const listOfPeople = props.list
-  if(listOfPeople.length)
-  {
-      const curate = listOfPeople.filter((info)=>{
-          return info.visible === true
-      })
-       const list = curate.map((info)=>{
-               
-                    return <p key = {info.name}>{info.name}  {info.number}</p>
-                })
-      return(
-          <div>
-             {
-               list
-             }
-          </div>
-          
-      )
-  }
-  else{return <></>}
+const Persons = (props) => {
+    const listOfPeople = props.list
+    if (listOfPeople.length) {
+        const curate = listOfPeople.filter((info) => {
+            return info.visible === true
+        })
+        const list = curate.map((info) => {
+
+            return <p key={info.name}>{info.name}  {info.number}</p>
+        })
+        return (
+            <div>
+                {
+                    list
+                }
+            </div>
+
+        )
+    }
+    else { return <></> }
 }
-const Filter = ({searchField, handleSearchField})=>{
-    return(
+const Filter = ({ searchField, handleSearchField }) => {
+    return (
         <div>
-            form shown with <input value = {searchField} onChange = {handleSearchField}/>
+            form shown with <input value={searchField} onChange={handleSearchField} />
         </div>
     )
 }
-const PersonFomr = ()=>{
-    
+const PersonForm = ({newName,newNumber, event, handleNameSender, handleNumberSender, handleSubmitSender}) => {
+   return(
+    <form >
+    <div>
+        name: <input value={newName} onChange={(event) => handleNameSender(event)} />
+    </div>
+    <div>number: <input value={newNumber} onChange={(event) => handleNumberSender(event)} /></div>
+    <div>
+        <button type="submit" onClick={(event) => handleSubmitSender(event)}>add</button>
+    </div>
+   </form>
+   )
 }
 const App = () => {
     const [persons, setPersons] = useState([
@@ -37,81 +46,80 @@ const App = () => {
         { name: 'Ada Lovelace', number: '39-44-5323523', visible: true },
         { name: 'Dan Abramov', number: '12-43-234345', visible: true },
         { name: 'Mary Poppendieck', number: '39-23-6423122', visible: true }
-      ])
-  const [newNumber, setNewNumber] = useState('')
-  const [ newName, setNewName ] = useState('')
-  const [searchField, setSearch] = useState('')
-const handleName = (event)=>{
-   
-    //I got an error about uncontrolled input and controlled input unitil I updated the state
-     setNewName(event.target.value)
- 
-}
-const handleNumber = (event)=>{
-    event.preventDefault()
-    setNewNumber(event.target.value)
-}
-const handleSubmit = (event)=>{
-    
-  event.preventDefault()
-   const listPeople = [...persons]
+    ])
+    const [newNumber, setNewNumber] = useState('')
+    const [newName, setNewName] = useState('')
+    const [searchField, setSearch] = useState('')
+    const handleName = (event) => {
 
-   //Directly checking includes of an array of objects is not possible
-   const listNames = listPeople.map((info)=>info.name)
-   
-   if(listNames.includes(newName))
-   {
-       alert(`This guy ${newName} is already exists`)
-   } 
-   else{
-    const data = {name: newName, number: newNumber, visible: true}
-    listPeople.push(data)
-    setPersons(listPeople)
-   }
-   
-}
-const handleSearchChange = (event)=>{
-    event.preventDefault()
-   
-    const listPeople = [...persons]
-    
-    setSearch(event.target.value)
-    const listToDisplay = listPeople.map((info)=>{
-        var name = info.name
-        //Setting event.target.value works but searchField is not working it's always 1 step behind the time
-        //Don't know why
-        if(!name.includes(event.target.value))
-        {
-            info.visible = false
+        //I got an error about uncontrolled input and controlled input unitil I updated the state
+        setNewName(event.target.value)
+
+    }
+    const handleNumber = (event) => {
+        event.preventDefault()
+        setNewNumber(event.target.value)
+    }
+    const handleSubmit = (event) => {
+
+        event.preventDefault()
+        const listPeople = [...persons]
+
+        //Directly checking includes of an array of objects is not possible
+        const listNames = listPeople.map((info) => info.name)
+
+        if (listNames.includes(newName)) {
+            alert(`${newName} is already added to the PhoneBook`)
+        }
+        else {
+            const data = { name: newName, number: newNumber, visible: true }
+            listPeople.push(data)
+            setPersons(listPeople)
+        }
+
+    }
+    const handleSearchChange = (event) => {
+        event.preventDefault()
+
+        const listPeople = [...persons]
+
+        setSearch(event.target.value)
+        const listToDisplay = listPeople.map((info) => {
+            var name = info.name.toLowerCase()
+            //Setting event.target.value works but searchField is not working it's always 1 step behind the time
+            //That's why better to use event.target.value
+           
+            if (!name.includes(event.target.value.toLowerCase())) {
+                info.visible = false
+
+            }
+            else {
+                info.visible = true
+            }
+            return info
+        })
+
+        setPersons(listToDisplay)
+    }
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Filter searchField={searchField} handleSearchField={(event) => handleSearchChange(event)} />
+
+            <h2>Add a new </h2>
             
-        }
-        else
-        {
-            info.visible = true
-        }
-        return info
-    })
-     
-    setPersons(listToDisplay)    
-}
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter searchField = {searchField} handleSearchField ={(event)=>handleSearchChange(event)}/>
-      <form >
-        <div>
-          name: <input value = {newName} onChange = {(event)=>handleName(event)}/>
+            <PersonForm  
+            newNmae = {newName}
+            newNumber = {newNumber} 
+            handleNameSender = {(event)=>handleName(event)}
+            handleNumberSender = {(event)=>handleNumber(event)}
+            handleSubmitSender = {(event)=>handleSubmit(event)}
+            />
+            <h2>Numbers</h2>
+            <Persons list={persons} />
+
         </div>
-        <div>number: <input value = {newNumber} onChange = {(event)=>handleNumber(event)}/></div>
-        <div>
-          <button type="submit" onClick = {(event)=>handleSubmit(event)}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-        <Persons list = {persons}/>
-     
-    </div>
-  )
+    )
 }
 
 export default App
